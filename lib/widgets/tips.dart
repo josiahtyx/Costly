581e17f1e20739501fc51ad4d1772a4cd134dfac
Josiah Tan <josiah.t.y.x@gmail.com>
2022-12-29 22:42:56 +0200

    Removed redundant files, new quotes

--- lib/widgets/tips.dart
+++ lib/widgets/tips.dart
@@ -7,11 +7,12 @@ class userTips extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List<String> tips = [
-      "Did you know owning a 999€ device \nonly costs 1€ a day?",
-      "Finance without strategy is just numbers, \nand strategy without finance \nis just dreaming. \n~ E. Faber",
+      "Did you know owning a 1000€ device \nfor 3 years costs 0,91€ a day?",
+      "Finance without strategy is just numbers, \nand strategy without finance is just dreaming. \n~ E. Faber",
       "Money, like emotions, is something \nyou must control to keep \nyour life on the right track. \n~ Natasha Munson",
       "Financial freedom is a mental, \nemotional and educational process. \n~ Robert Kiyosaki",
       "A budget is telling your money \nwhere to go instead of \nwondering where it went. \n~ Dave Ramsey",
+      "You must gain control over your money \nor the lack of it will forever control you. \n~ Dave Ramsey"
     ];
     tips.shuffle();
     print(tips);
